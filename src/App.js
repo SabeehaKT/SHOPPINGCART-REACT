b359819ca0a69f52aafb942b3debbb8b03c7d390
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-08 02:10:57 +0530

    ShopingCartApp with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddCart from './Components/AddCart';
+import SearchCart from './Components/SearchCart';
+import DeleteCart from './Components/DeleteCart';
+import ViewAll from './Components/ViewAll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddCart/>}/>
+        <Route path="/search" element={<SearchCart/>}/>
+        <Route path="/delete" element={<DeleteCart/>}/>
+        <Route path="/view" element={<ViewAll/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
